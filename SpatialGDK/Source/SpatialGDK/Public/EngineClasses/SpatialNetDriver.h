c1755efc143467cdfc9c74e046d8cd1de3b2fed1
Ben Naccarato <bennaccarato@improbable.io>
2019-01-09 12:11:41 +0000

    Send delayed delete entity request if tearing off

--- SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialNetDriver.h
+++ SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialNetDriver.h
@@ -151,9 +151,11 @@ private:
 	TUniquePtr<FSpatialOutputDevice> SpatialOutputDevice;
 
 	TMap<Worker_EntityId_Key, USpatialActorChannel*> EntityToActorChannel;
-
+public:
 	// Timer manager.
 	FTimerManager* TimerManager;
+
+private:
 
 	bool bAuthoritativeDestruction;
 	bool bConnectAsClient;
